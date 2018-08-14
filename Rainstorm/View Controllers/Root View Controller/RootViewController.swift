6fd81809fcb66873932c207459a8537ebc21645e
Nathan Standage <developer@nathanstandage.com>
2018-08-14 17:14:21 -0600

    Project setup

---
+++ Rainstorm/View Controllers/Root View Controller/RootViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RootViewController.swift
+//  Rainstorm
+//
+//  Created by Nathan Standage on 8/14/18.
+//  Copyright © 2018 Nathan Standage. All rights reserved.
+//
+
+import UIKit
+
+final class RootViewController: UIViewController {
+
+    //MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
